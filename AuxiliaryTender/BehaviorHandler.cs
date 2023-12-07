e6d489cfdba0abc84a54d67212880a21e46af6f0
Brian Engelhardt <lassombra@live.com>
2023-12-06 22:12:01 -0600

    Water indicator working

--- AuxiliaryTender/BehaviorHandler.cs
+++ AuxiliaryTender/BehaviorHandler.cs
@@ -3,6 +3,8 @@ using DV.CabControls.Spec;
 using DV.Damage;
 using DV.Logic.Job;
 using DV.Simulation.Cars;
+using DV.Simulation.Controllers;
+using DV.Simulation.Ports;
 using DV.ThingTypes;
 using LocoSim.Attributes;
 using LocoSim.Definitions;
@@ -50,68 +52,96 @@ namespace AuxiliaryTender
 			Main.Logger?.Log("Aux tender found, patching");
 			(from l in Globals.G.Types.Liveries
 			 where l.prefab != null && Constants.validTankNames.Any(name => l.prefab.name.Contains(name))
-			 select l).ToList().ForEach(livery =>
+			 select l).ToList<TrainCarLivery>().ForEach(livery =>
 			 {
-				 var prefab = livery.prefab;
-				 Main.Logger?.Log("Patching prefab " + prefab.name);
-				 var waterBox = FindRecursive(prefab.transform, "[WaterCollider]");
-				 Main.Logger?.Log("waterBox found " + waterBox);
-				 var receiver = waterBox?.gameObject.AddComponent<LocoResourceReceiver>();
-				 if (receiver != null && waterBox != null)
-				 {
-					 receiver.resourceType = ResourceType.Water;
-					 waterBox.gameObject.tag = "LocoResourceReceiver";
-				 }
-				 var damageController = prefab.gameObject.AddComponent<DamageController>();
-				 foreach (var item in damageController.GetType().GetFields())
-				 {
-					 if (item.CustomAttributes.Any(attr => attr.AttributeType == typeof(PortIdAttribute)))
-					 {
-						 item.SetValue(damageController, new string[] { });
-					 }
-				 }
-				 var waterContainer = prefab.gameObject.AddComponent<WaterContainerDefinition>();
-				 waterContainer.ID = "auxWater";
-				 waterContainer.capacity = 45000f;
-				 waterContainer.defaultValue = waterContainer.capacity;
-				 var simConnections = prefab.gameObject.AddComponent<SimConnectionDefinition>();
-				 simConnections.executionOrder = new SimComponentDefinition[]
-				 {
-					waterContainer
-				 };
-				 simConnections.connections = new Connection[0];
-				 simConnections.portReferenceConnections = new PortReferenceConnection[0];
-				 var simController = prefab.gameObject.AddComponent<SimController>();
-				 simController.connectionsDefinition = simConnections;
-				 simController.otherSimControllers = new DV.Simulation.Controllers.ASimInitializedController[0];
-				 prefab.gameObject.AddComponent<WaterModule>();
-				 var externalInteractions = livery.externalInteractablesPrefab;
-				 var hatch = FindRecursive(externalInteractions.transform, "AxTenderHatch")?.gameObject;
-				 if (hatch != null)
-				 {
-					 var lever = hatch.AddComponent<Lever>();
-					 lever.rigidbodyMass = 30;
-					 lever.rigidbodyDrag = 4;
-					 lever.rigidbodyAngularDrag = 0;
-					 lever.blockAngularDrag = 0;
-					 lever.blockDrag = 0;
-					 lever.maxForceAppliedMagnitude = float.PositiveInfinity;
-					 lever.scrollWheelSpring = 0;
-					 lever.notches = 2;
-					 lever.jointAxis = Vector3.right;
-					 lever.useSpring = true;
-					 lever.jointSpring = 175;
-					 lever.jointDamper = 17.5f;
-					 lever.useLimits = true;
-					 lever.jointLimitMin = 0;
-					 lever.jointLimitMax = 160;
-					 lever.colliderGameObjects = new GameObject[] { hatch.transform.Find("[colliders]").gameObject };
-					 hatch.transform.Find("water fill blocker").gameObject.layer = 15; // force this one to layer 15 so it can prevent filling.
-				 }
-				 Main.Logger?.Log("Sim Controller created and initialized for prefab " + prefab.name);
+				 AttachWaterResource(livery);
+				 AttachHatch(livery);
+				 AttachWaterIndicator(livery);
+				 Main.Logger?.Log("Sim Controller created and initialized for prefab " + livery.prefab.name);
 			 });
 			TerminateAttach();
 		}
+
+		private static void AttachWaterIndicator(TrainCarLivery livery)
+		{
+			GameObject? water = FindRecursive(livery.prefab.transform, "AxTenderWater")?.gameObject;
+			if (water != null)
+			{
+				Main.Logger?.Log("Water object found, attaching indicator");
+				var scaler = water.AddComponent<IndicatorScaler>();
+				scaler.startScale = new Vector3(1.0f, 0.0f, 1.0f);
+				scaler.minValue = 0.0f;
+				var feeder = water.AddComponent<IndicatorPortReader> ();
+				feeder.portId = "auxWater.NORMALIZED";
+				var controller = livery.prefab.gameObject.AddComponent<IndicatorPortReadersController>();
+				controller.entries = new IndicatorPortReader[] { feeder };
+			}
+		}
+
+		private static void AttachWaterResource(TrainCarLivery livery)
+		{
+			var prefab = livery.prefab;
+			Main.Logger?.Log("Patching prefab " + prefab.name);
+			var waterBox = FindRecursive(prefab.transform, "[WaterCollider]");
+			Main.Logger?.Log("waterBox found " + waterBox);
+			var receiver = waterBox?.gameObject.AddComponent<LocoResourceReceiver>();
+			if (receiver != null && waterBox != null)
+			{
+				receiver.resourceType = ResourceType.Water;
+				waterBox.gameObject.tag = "LocoResourceReceiver";
+			}
+			var damageController = prefab.gameObject.AddComponent<DamageController>();
+			foreach (var item in damageController.GetType().GetFields())
+			{
+				if (item.CustomAttributes.Any(attr => attr.AttributeType == typeof(PortIdAttribute)))
+				{
+					item.SetValue(damageController, new string[] { });
+				}
+			}
+			var waterContainer = prefab.gameObject.AddComponent<WaterContainerDefinition>();
+			waterContainer.ID = "auxWater";
+			waterContainer.capacity = 45000f;
+			waterContainer.defaultValue = waterContainer.capacity;
+			var simConnections = prefab.gameObject.AddComponent<SimConnectionDefinition>();
+			simConnections.executionOrder = new SimComponentDefinition[]
+			{
+					waterContainer
+			};
+			simConnections.connections = new Connection[0];
+			simConnections.portReferenceConnections = new PortReferenceConnection[0];
+			var simController = prefab.gameObject.AddComponent<SimController>();
+			simController.connectionsDefinition = simConnections;
+			simController.otherSimControllers = new DV.Simulation.Controllers.ASimInitializedController[0];
+			prefab.gameObject.AddComponent<WaterModule>();
+		}
+
+		private static void AttachHatch(TrainCarLivery livery)
+		{
+			var externalInteractions = livery.externalInteractablesPrefab;
+			var hatch = FindRecursive(externalInteractions.transform, "AxTenderHatch")?.gameObject;
+			if (hatch != null)
+			{
+				var lever = hatch.AddComponent<Lever>();
+				lever.rigidbodyMass = 30;
+				lever.rigidbodyDrag = 4;
+				lever.rigidbodyAngularDrag = 0;
+				lever.blockAngularDrag = 0;
+				lever.blockDrag = 0;
+				lever.maxForceAppliedMagnitude = float.PositiveInfinity;
+				lever.scrollWheelSpring = 0;
+				lever.notches = 2;
+				lever.jointAxis = Vector3.right;
+				lever.useSpring = true;
+				lever.jointSpring = 175;
+				lever.jointDamper = 17.5f;
+				lever.useLimits = true;
+				lever.jointLimitMin = 0;
+				lever.jointLimitMax = 160;
+				lever.colliderGameObjects = new GameObject[] { hatch.transform.Find("[colliders]").gameObject };
+				hatch.transform.Find("water fill blocker").gameObject.layer = 15; // force this one to layer 15 so it can prevent filling.
+			}
+		}
+
 		private static Transform? FindRecursive(Transform source, string name)
 		{
 			Transform? child = null;

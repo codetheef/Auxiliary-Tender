3e14cc5b571af55fd95f6ea6e04676fd759a22ba
Brian Engelhardt <lassombra@live.com>
2023-12-12 21:25:37 -0600

    Set up Resource Container

--- visualStudio/AuxiliaryTenderTypes/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AuxiliaryTenderTypes
-{
-	public class Class1
-	{
-
-	}
-}
---
+++ visualStudio/AuxiliaryTenderTypes/DVTypeEquivalents/ResourceType.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxiliaryTenderTypes.DVTypeEquivalents
+{
+	public enum ResourceType
+	{
+		FUEL,
+		// Token: 0x04000003 RID: 3
+		SAND,
+		// Token: 0x04000004 RID: 4
+		OIL,
+		// Token: 0x04000005 RID: 5
+		WATER,
+		// Token: 0x04000006 RID: 6
+		COAL
+	}
+}
